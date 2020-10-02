8d86bcff4c3320bd8897d28f7cceb9f6ead49c2b
sammymutahi12@gmail.com <sammymutahi12@gmail.com>
2020-10-02 14:08:20 +0300

    fix menu

--- movies/src/main/java/com/sammy/movies/ui/MoviesFragment.kt
+++ movies/src/main/java/com/sammy/movies/ui/MoviesFragment.kt
@@ -1,9 +1,8 @@
 package com.sammy.movies.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.PopupMenu
 import android.widget.Toolbar
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -13,6 +12,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.sammy.datasource.cache.movies.MoviesResponse
 import com.sammy.movies.R
 import com.sammy.movies.utils.POPULAR
+import com.sammy.movies.utils.TOP_RATED
+import com.sammy.movies.utils.UPCOMING
 import dagger.android.support.DaggerFragment
 import kotlinx.android.synthetic.main.fragment_movies.*
 import javax.inject.Inject
@@ -25,6 +26,14 @@ class MoviesFragment : DaggerFragment() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
+    private var sortBy = POPULAR
+    private var isFetchingMovies:Boolean = false
+    private var currentPage = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
 
     override fun onCreateView(
@@ -43,15 +52,49 @@ class MoviesFragment : DaggerFragment() {
 
         initViewModel()
         initObservers()
-        getData(1, POPULAR)
+        getData(currentPage)
+        setupOnScrollListener()
+
     }
-    private fun getData(page:Int,sortBy:String) {
+    private fun setupOnScrollListener() {
+        val manager = LinearLayoutManager(context)
+        movies_list.setLayoutManager(manager)
+        movies_list.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val totalItemCount = manager.itemCount
+                val visibleItemCount = manager.childCount
+                val firstVisibleItem = manager.findFirstVisibleItemPosition()
+                if (firstVisibleItem + visibleItemCount >= totalItemCount / 2) {
+                    if (!isFetchingMovies) {
+                        getData(currentPage + 1)
+                    }
+                }
+            }
+        })
+    }
+
+    private fun getData(page:Int) {
+        isFetchingMovies = true
         moviesViewModel.getMovies(page,sortBy).observe(this, Observer {
             if(it != null){
                 initRecyclerView(it)
+                isFetchingMovies = false
+                currentPage = page
+                setTitle()
             }
 
         })
+    }
+
+    private fun setTitle() {
+        if (sortBy == POPULAR){
+            activity!!.title = "Popular"
+        }else if(sortBy == TOP_RATED){
+            activity!!.title = "Top Rated"
+        }else{
+            activity!!.title = "Upcoming"
+        }
     }
 
 
@@ -75,6 +118,51 @@ class MoviesFragment : DaggerFragment() {
         val layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
         movies_list.layoutManager = layoutManager
         movies_list.adapter = adapter
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu, menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(R.id.sort == item.itemId){
+            showSortMenu()
+            return true
+        }else{
+            return super.onOptionsItemSelected(item)
+        }
+
+    }
+
+    private fun showSortMenu() {
+        val sortMenu =
+            PopupMenu(context,activity!!.findViewById(R.id.sort))
+        sortMenu.setOnMenuItemClickListener { item ->
+            currentPage = 1
+            when (item.itemId) {
+                R.id.popular -> {
+                    sortBy = POPULAR
+                    getData(currentPage)
+                    true
+                }
+                R.id.top_rated -> {
+                    sortBy = TOP_RATED
+                    getData(currentPage)
+                    true
+                }
+                R.id.upcoming -> {
+                    sortBy = UPCOMING
+                    getData(currentPage)
+                    true
+                }
+                else -> false
+            }
+        }
+        sortMenu.inflate(R.menu.menu_movies_sort)
+        sortMenu.show()
     }
 
 }
