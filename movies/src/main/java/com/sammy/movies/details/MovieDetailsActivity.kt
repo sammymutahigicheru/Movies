44d79df898b0871482ab7710df61040ea76d03c6
sammymutahi12@gmail.com <sammymutahi12@gmail.com>
2020-10-06 19:36:20 +0300

    display movie details

--- movies/src/main/java/com/sammy/movies/details/MovieDetailsActivity.kt
+++ movies/src/main/java/com/sammy/movies/details/MovieDetailsActivity.kt
@@ -1,18 +1,39 @@
 package com.sammy.movies.details
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.sammy.datasource.cache.movies.Movie
 import com.sammy.movies.R
+import com.sammy.movies.utils.IMAGE_BASE_URL
+import kotlinx.android.synthetic.main.activity_movie_details.*
 
 class MovieDetailsActivity : AppCompatActivity() {
     lateinit var movie:Movie
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
-        movie = intent.getParcelableExtra<Movie>("movie")!!
+        movie = intent.getParcelableExtra("movie")!!
         setupToolbar()
+        initItems()
+    }
+
+    private fun initItems() {
+        movieDetailsTitle.text = movie.title
+        summaryLabel.visibility= View.VISIBLE
+        movieDetailsOverview.text = movie.overview
+        movieDetailsRating.visibility =  View.VISIBLE
+        movieDetailsRating.rating = movie.rating!!.div(2)
+        movieDetailsReleaseDate.text = movie.releaseDate
+        if (!isFinishing) {
+            Glide.with(this)
+                .load("https://image.tmdb.org/t/p/w500${movie.backdrop}")
+                .apply(RequestOptions.placeholderOf(R.color.colorPrimary))
+                .into(movieDetailsBackdrop)
+        }
     }
 
     private fun setupToolbar() {
