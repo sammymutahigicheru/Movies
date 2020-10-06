1e4a8c58584860bedca45e1464b55063a654afc5
sammymutahi12@gmail.com <sammymutahi12@gmail.com>
2020-10-06 18:28:10 +0300

    set up toolbar in movies details activity

--- movies/src/main/java/com/sammy/movies/details/MovieDetailsActivity.kt
+++ movies/src/main/java/com/sammy/movies/details/MovieDetailsActivity.kt
@@ -1,12 +1,22 @@
 package com.sammy.movies.details
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
 import com.sammy.movies.R
 
 class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
+        setupToolbar()
+    }
+    private fun setupToolbar() {
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setActionBar(toolbar)
+        if (supportActionBar != null) {
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+            supportActionBar!!.setDisplayShowTitleEnabled(false)
+        }
     }
 }
