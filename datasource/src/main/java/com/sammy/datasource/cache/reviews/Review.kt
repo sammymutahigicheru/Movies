887e569e5534fd55f189065f634586628721c9b0
sammymutahi12@gmail.com <sammymutahi12@gmail.com>
2020-10-08 16:26:08 +0300

    refactor datasource module(trailers and reviews pojos

--- datasource/src/main/java/com/sammy/datasource/cache/reviews/Review.kt
+++ datasource/src/main/java/com/sammy/datasource/cache/reviews/Review.kt
@@ -1,11 +1,8 @@
 package com.sammy.datasource.cache.reviews
 
-import android.os.Parcelable
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
 
-@Parcelize
 data class Review(
     @SerializedName("author") @Expose
     private var author: String? = null,
@@ -13,4 +10,4 @@ data class Review(
     @SerializedName("content")
     @Expose
     private val content: String? = null
-):Parcelable
+)
--- datasource/src/main/java/com/sammy/datasource/cache/trailer/Trailer.kt
+++ datasource/src/main/java/com/sammy/datasource/cache/trailer/Trailer.kt
@@ -1,12 +1,9 @@
 package com.sammy.datasource.cache.trailer
 
-import android.os.Parcelable
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
 
-@Parcelize
 data class Trailer(
     @SerializedName("key") @Expose
     private var key: String? = null
-):Parcelable
+)
