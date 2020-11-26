a9dc676188130d6f6cd093b76fc688789d9d3410
sammymutahigicheru <sammymutahi12@gmail.com>
2020-11-26 19:16:29 +0300

    add datasource module

--- app/src/main/java/com/sammy/movies/di/DataSourceModule.kt
+++ app/src/main/java/com/sammy/movies/di/DataSourceModule.kt
@@ -1,2 +1,14 @@
 package com.sammy.movies.di
 
+import com.sammy.datasource.datasource.GenreDataSource
+import com.sammy.datasource.datasource.MovieDataSource
+import com.sammy.datasource.datasource.ReviewDatasource
+import com.sammy.datasource.datasource.TrailerDatasource
+import org.koin.dsl.module
+
+val dataSourceModule = module {
+    single { GenreDataSource(get(),get()) }
+    single { MovieDataSource(get(),get()) }
+    single { ReviewDatasource(get(),get()) }
+    single { TrailerDatasource(get(),get()) }
+}
